76b5e6661cdf9921aa27329452a5cc3dcb06cf06
Mark Gerarts <mark.gerarts@gmail.com>
2023-07-02 09:57:04 +0200

    Implement exercise I.3

--- src/App.fs
+++ src/App.fs
@@ -4,10 +4,12 @@ open P5.Core
 open NatureOfCode
 
 let private sketches =
-    dict["Introduction/Example01/TraditionalRandomWalk", Introduction.Example01.TraditionalRandomWalk.run
-         "Introduction/Example02/RandomNumberDistribution", Introduction.Example02.RandomNumberDistribution.run
-         "Introduction/Example03/RightWalker", Introduction.Example03.RightWalker.run
-         "Introduction/Exercise01/DownRightWalker", Introduction.Exercise01.DownRightWalker.run]
+    [ "Introduction/Example01/TraditionalRandomWalk", Introduction.Example01.TraditionalRandomWalk.run
+      "Introduction/Example02/RandomNumberDistribution", Introduction.Example02.RandomNumberDistribution.run
+      "Introduction/Example03/RightWalker", Introduction.Example03.RightWalker.run
+      "Introduction/Exercise01/DownRightWalker", Introduction.Exercise01.DownRightWalker.run
+      "Introduction/Exercise03/DynamicRandomWalker", Introduction.Exercise03.DynamicRandomWalker.run ]
+    |> Map.ofList
 
 let runSketch name canvasSelector =
     let node = Element <| Browser.Dom.document.querySelector (canvasSelector)
@@ -17,4 +19,4 @@ let runSketch name canvasSelector =
 
     sketches[name]node
 
-let allNames = sketches.Keys |> Seq.toArray
+let allNames = sketches |> Map.toSeq |> Seq.map fst |> Seq.toArray
---
+++ src/Introduction/Exercise03/DynamicRandomWalker.fs
@@ -0,0 +1,49 @@
+//! Exercise I.3: Dynamic random walker
+module NatureOfCode.Introduction.Exercise03.DynamicRandomWalker
+
+open P5.Core
+open P5.Environment
+open P5.Color
+open P5.Shape
+open P5.Rendering
+open P5.Math
+open P5.Events
+
+type Walker = { x: int; y: int }
+
+let random = new System.Random()
+
+let setup p5 =
+    createCanvas p5 720 400
+    background p5 (Grayscale 255)
+    stroke p5 (Grayscale 0)
+
+    { x = width p5 / 2; y = height p5 / 2 }
+
+let randomStep p5 walker =
+    let choices = [| -1; 0; 1 |]
+    let choiceX = randomChoice p5 choices
+    let choiceY = randomChoice p5 choices
+
+    { x = walker.x + choiceX
+      y = walker.y + choiceY }
+
+let toMouseStep p5 walker =
+    let mouseX = mouseX p5 |> int
+    let mouseY = mouseY p5 |> int
+
+    let deltaX = if walker.x < mouseX then 1 else -1
+    let deltaY = if walker.y < mouseY then 1 else -1
+
+    { x = walker.x + deltaX
+      y = walker.y + deltaY }
+
+let update p5 walker =
+    let action = if random.Next(2) = 0 then randomStep else toMouseStep
+    action p5 walker
+
+let draw p5 walker =
+    // Adding .5 yields better results with regards to aliasing.
+    point p5 (float walker.x + 0.5) (float walker.y + 0.5)
+
+let run node = simulate node setup update draw
